3766a7c4e0d441e47371a9ec72adb5833eaa3418
tmsitw <ilyuha_t@yahoo.com>
2019-11-18 18:47:23 +0300

    controller and test

---
+++ src/test/java/com/example/demo/GetHelloTest.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import com.example.demo.controller.HelloController;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
+@SpringBootTest
+public class GetHelloTest {
+    HelloController controller = new HelloController();
+
+    @Test
+    void getHelloTest() {
+        assertTrue("I`am not greeting you!", controller.getHello().equals("Hello world!"));
+    }
+
+}
